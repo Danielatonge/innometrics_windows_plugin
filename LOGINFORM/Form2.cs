872e8189c1dd4571d587f4292809fe84305e1ccc
Daniel Atonge <elamboatonge@gmail.com>
2019-10-07 23:47:26 +0300

    integrates basic functionalities

---
+++ LOGINFORM/Form2.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Management;
+using System.Dynamic;
+using System.Net.Http;
+using static System.Windows.Forms.ListView;
+
+namespace LOGINFORM
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        public async Task renderProcessesOnListView()
+        {
+            HashSet<string> hSet = new HashSet<string>();
+            while (true)
+            {
+                // Create an array to store the processes
+                Process[] processList = Process.GetProcesses();
+
+                // Create an Imagelist that will store the icons of every process
+                ImageList Imagelist = new ImageList();
+
+                // Loop through the array of processes to show information of every process in your console
+
+                foreach (Process process in processList)
+                {
+
+                    if (!hSet.Contains(process.Id.ToString()))
+                    {
+                        // Define the status from a boolean to a simple string
+                        string status = (process.Responding == true ? "Responding" : "Not responding");
+
+                        // Retrieve the object of extra information of the process (to retrieve Username and Description)
+                        dynamic extraProcessInfo = GetProcessExtraInformation(process.Id);
+
+                        int condition = (process.ProcessName.CompareTo("Idle"));
+                        // Create an array of string that will store the information to display in our 
+                        string[] row = {
+                        // 1 Process name
+                        process.ProcessName,
+                        // 2 Process ID
+                        process.Id.ToString(),
+                        // 3 Process status
+                        status,
+                        // 4 Username that started the process
+                        extraProcessInfo.Username,
+                         // 5 Time associated process was started
+                        (condition != 0)?process.StartTime.ToString(): "",
+                        // 6 Memory usage
+                        BytesToReadableValue(process.PrivateMemorySize64),
+                   
+                        // 7 Total processor time for this process
+                        (condition != 0)?process.TotalProcessorTime.ToString(): "",
+                        // 8 Description of the process
+                        extraProcessInfo.Description
+                     };
+
+
+                        //Console.WriteLine(process.ProcessName);
+                        //Console.WriteLine("D_A_N_I_E_L"+process.StartTime.ToString());
+                        //Console.WriteLine(process.TotalProcessorTime.ToString());
+                        //
+                        // As not every process has an icon then, prevent the app from crash
+                        try
+                        {
+                            Imagelist.Images.Add(
+                                // Add an unique Key as identifier for the icon (same as the ID of the process)
+                                process.Id.ToString(),
+                                // Add Icon to the List 
+                                Icon.ExtractAssociatedIcon(process.MainModule.FileName).ToBitmap()
+                            );
+                        }
+                        catch { }
+
+                        // Create a new Item to add into the list view that expects the row of information as first argument
+                        ListViewItem item = new ListViewItem(row)
+                        {
+                            // Set the ImageIndex of the item as the same defined in the previous try-catch
+                            ImageIndex = Imagelist.Images.IndexOfKey(process.Id.ToString())
+                        };
+
+
+
+                        // Add the Item
+
+                        hSet.Add(process.Id.ToString());
+                        Metrics.Items.Add(item);
+                    }
+                }
+
+                // Set the imagelist of your list view the previous created list :)
+                Metrics.LargeImageList = Imagelist;
+                Metrics.SmallImageList = Imagelist;
+
+                await Task.Delay(8000);
+            }
+        }
+
+        /// <summary>
+        /// Method that converts bytes to its human readable value
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        public string BytesToReadableValue(long number)
+        {
+            List<string> suffixes = new List<string> { " B", " KB", " MB", " GB", " TB", " PB" };
+
+            for (int i = 0; i < suffixes.Count; i++)
+            {
+                long temp = number / (int)Math.Pow(1024, i + 1);
+
+                if (temp == 0)
+                {
+                    return (number / (int)Math.Pow(1024, i)) + suffixes[i];
+                }
+            }
+
+            return number.ToString();
+        }
+
+        /// <summary>
+        /// Returns an Expando object with the description and username of a process from the process ID.
+        /// </summary>
+        /// <param name="processId"></param>
+        /// <returns></returns>
+        public ExpandoObject GetProcessExtraInformation(int processId)
+        {
+            // Query the Win32_Process
+            string query = "Select * From Win32_Process Where ProcessID = " + processId;
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(query);
+            ManagementObjectCollection processList = searcher.Get();
+
+            // Create a dynamic object to store some properties on it
+            dynamic response = new ExpandoObject();
+            response.Description = "";
+            response.Username = "Unknown";
+
+            foreach (ManagementObject obj in processList)
+            {
+                // Retrieve username 
+                string[] argList = new string[] { string.Empty, string.Empty };
+                int returnVal = Convert.ToInt32(obj.InvokeMethod("GetOwner", argList));
+                if (returnVal == 0)
+                {
+                    // return Username
+                    response.Username = argList[0];
+
+                    // You can return the domain too like (PCDesktop-123123\Username using instead
+                    //response.Username = argList[1] + "\\" + argList[0];
+                }
+
+                // Retrieve process description if exists
+                if (obj["ExecutablePath"] != null)
+                {
+                    try
+                    {
+                        FileVersionInfo info = FileVersionInfo.GetVersionInfo(obj["ExecutablePath"].ToString());
+                        response.Description = info.FileDescription;
+                    }
+                    catch { }
+                }
+            }
+
+            return response;
+        }
+
+        private void PictureBox2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            renderProcessesOnListView();
+        
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            int number_of_items = Metrics.Items.Count;
+            ListViewItemCollection items = Metrics.Items;
+            for (int i = 0; i < 3; i++)
+            {
+                string activity = items[i].SubItems[3].ToString();
+                string start = items[i].SubItems[4].ToString();
+                string end = items[i].SubItems[5].ToString();
+                string name = items[i].SubItems[0].ToString();
+                string ip_address = items[i].SubItems[1].ToString();
+                string mac = items[i].SubItems[6].ToString();
+                string data = $"activity={activity}&start_time={start}&end_time={end}&executable_name={name}&ip_address={ip_address}&mac_address={mac}";
+                MyWebRequest myRequest = new MyWebRequest("https://innometrics-12856.firebaseapp.com/activity", "POST",data);
+                MessageBox.Show(myRequest.GetResponse());
+            }
+            
+        }
+
+        private void Button7_Click(object sender, EventArgs e)
+        {
+            Metrics.Items.Clear();
+        }
+    }
+}
---
+++ LOGINFORM/Form3.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace LOGINFORM
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtSurname = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(195, 391);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(117, 57);
+            this.btnSubmit.TabIndex = 0;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.BtnSubmit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(43, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Email:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(43, 141);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 29);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(43, 194);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(124, 29);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Surname:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(43, 242);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(133, 29);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "password:";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(195, 94);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(238, 22);
+            this.txtEmail.TabIndex = 5;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(195, 145);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(238, 22);
+            this.txtName.TabIndex = 6;
+            // 
+            // txtSurname
+            // 
+            this.txtSurname.Location = new System.Drawing.Point(195, 196);
+            this.txtSurname.Name = "txtSurname";
+            this.txtSurname.Size = new System.Drawing.Size(238, 22);
+            this.txtSurname.TabIndex = 7;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(195, 247);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(238, 22);
+            this.txtPassword.TabIndex = 8;
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(510, 477);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtSurname);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSubmit);
+            this.Name = "Form3";
+            this.Text = "Form3";
+            this.Load += new System.EventHandler(this.Form3_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtSurname;
+        private System.Windows.Forms.TextBox txtPassword;
+    }
+}
